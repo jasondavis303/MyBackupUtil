9ea9970db3f0feb8fcdb95ee35fcd16d42965bac
Jason Davis <jasondavis303@gmail.com>
2025-05-20 17:02:56 -0600

    Code cleanup

--- MyBackupUtil/AppUpdater.cs
+++ MyBackupUtil/AppUpdater.cs
@@ -3,7 +3,6 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
-using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 
 namespace MyBackupUtil;
